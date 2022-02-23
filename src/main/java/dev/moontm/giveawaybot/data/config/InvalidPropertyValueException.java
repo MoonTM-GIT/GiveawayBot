09b8bf5177cfac9327f830ac952d1dbab613ed53
MoonTM <benzweitacc@gmail.com>
2022-02-23 01:03:17 +0100

    Removed InvalidPropertyValueException

--- src/main/java/dev/moontm/giveawaybot/data/config/InvalidPropertyValueException.java
+++
@@ -1,7 +0,0 @@
-package dev.moontm.giveawaybot.data.config;
-
-/**
- * Exception that is thrown when a invalid config property is requested.
- */
-public class InvalidPropertyValueException extends Exception {
-}
